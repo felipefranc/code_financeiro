43f5f1d143c4ed1227037a3c508ae7e318f955a8
Felipe <felipefrancvue@outlook.com>
2017-03-23 21:10:45 -0300

    Code Financeiro - Modulo principal - Webpack >
    Configurando BrowserSync e compilando Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,46 @@
+const gulp = require('gulp');
 const elixir = require('laravel-elixir');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const webpackConfig = require('./webpack.config');
+const webpackDevConfig = require('./webpack.dev.config');
 
 require('laravel-elixir-vue');
+require('laravel-elixir-webpack-official');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for your application as well as publishing vendor resources.
- |
- */
+//limpando loaders
+Elixir.webpack.config.module.loaders = [];
+
+Elixir.webpack.mergeConfig(webpackConfig);
+Elixir.webpack.mergeConfig(webpackDevConfig);
+
+
+gulp.task('webpack-dev-server', () => {
+    let config = Elixir.webpack.config;
+    new WebpackDevServer(webpack(config), {
+        proxy:{
+            '*': 'http://127.0.0.1:8080'
+        },
+        watchOptions: {
+            poll: true,
+            aggregateTimeout: 300
+        },
+        publicPath: config.output.publicPath,
+        noInfo: false,
+        stats: {colors: true}
+    }).listen(8000, "0.0.0.0", () => {
+        console.log("Bundling project...");
+    });
+});
 
 elixir((mix) => {
     mix.sass('./resources/assets/admin/sass/admin.scss')
-    .copy('./node_modules/materialize-css/fonts/roboto','./public/fonts/roboto');
+        .copy('./node_modules/materialize-css/fonts/roboto', './public/fonts/roboto');
+
+    gulp.start('webpack-dev-server');
 
     mix.browserSync({
         host: '0.0.0.0',
-        proxy: 'localhost:8080/code_financeiro'
+        proxy: 'http://127.0.0.1:8000'
     });
-       //.webpack('app.js');
 });
